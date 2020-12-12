848703e7a610f0fd7da4d206fbcfac31192e55d3
ialphaw <amirabbas.vaziri@gmail.com>
2020-12-12 12:38:48 +0330

    create model Relation for followers and followings

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 
 from account.models import Account
 
+
 class Profile(models.Model):
     user = models.OneToOneField(Account, on_delete=models.CASCADE)
     bio = models.TextField(null=True, blank=True)
@@ -19,3 +20,12 @@ def save_profile(sender, **kwargs):
     if kwargs['created']:
         p1 = Profile(user=kwargs['instance'])
         p1.save()
+
+
+class Relation(models.Model):
+    from_user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='follower')
+    to_user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='following')
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.from_user} following {self.to_user}'
